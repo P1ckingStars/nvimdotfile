b0825db8a0c67270bc62fa1aa27d614e1dce10f2
arthur <aw18611578009@gmail.com>
2025-04-20 20:33:42 -0400

    fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,12 +10,6 @@ return {
                 "sql", "verilog", "rust",
                 "llvm", "make", "cmake", "latex"
             },
-            sync_install     = false,
-            auto_install     = false,
-            highlight        = {
-                enable = true,
-                disable = { "latex" }
-            },
             indent           = { enable = true }
         })
     end
